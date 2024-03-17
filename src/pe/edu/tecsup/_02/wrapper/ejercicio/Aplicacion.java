873518cb1de98b22278836df9b72bfe02874b044
jgomezz <jgomezz@gmail.com>
2024-03-17 11:09:47 -0500

    Update

---
+++ src/pe/edu/tecsup/_02/wrapper/ejercicio/Aplicacion.java
@@ -0,0 +1,24 @@
+package pe.edu.tecsup._02.wrapper.ejercicio;
+
+public class Aplicacion {
+
+    public static void main(String[] args) {
+
+        // Float Double
+        
+        float dec = 1.2425f;
+        
+        Float fDec = Float.valueOf(dec);
+        
+        float decRep = fDec.floatValue();
+        
+        System.out.println("Decimal recuperado " + decRep);
+    
+        
+        // Boolean
+        
+        boolean estasEnClases = true;
+
+
+    }
+}
