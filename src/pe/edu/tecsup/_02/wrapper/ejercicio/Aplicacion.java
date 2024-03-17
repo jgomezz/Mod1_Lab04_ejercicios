70096e0a849db71c4e1076f24ba59f1b036689db
jgomezz <jgomezz@gmail.com>
2024-03-17 11:15:19 -0500

    Update

--- src/pe/edu/tecsup/_02/wrapper/ejercicio/Aplicacion.java
+++ src/pe/edu/tecsup/_02/wrapper/ejercicio/Aplicacion.java
@@ -6,19 +6,26 @@ public class Aplicacion {
 
         // Float Double
         
-        float dec = 1.2425f;
-        
-        Float fDec = Float.valueOf(dec);
-        
-        float decRep = fDec.floatValue();
-        
-        System.out.println("Decimal recuperado " + decRep);
+//        float dec = 1.2425f;
+//        
+//        Float fDec = Float.valueOf(dec);
+//        
+//        float decRep = fDec.floatValue();
+//        
+//        System.out.println("Decimal recuperado " + decRep);
     
         
         // Boolean
         
         boolean estasEnClases = true;
 
+        Boolean wrapperEstasEnClasess = Boolean.valueOf(estasEnClases);
+        
+        System.out.println("Boolean recuperado " + wrapperEstasEnClasess);
+
+        boolean recuperadoEstasEnClases = wrapperEstasEnClasess.booleanValue();
+        
+        System.out.println("boolean recuperado " + recuperadoEstasEnClases);
 
     }
 }
